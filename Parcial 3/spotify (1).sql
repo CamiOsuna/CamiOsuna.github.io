2706df297565c3f26d0ca0cc3b6bddade75fc247
CamiOsuna <159404147+CamiOsuna@users.noreply.github.com>
2024-06-04 17:54:53 -0700

    Practica 8 García Osuna Camila 04/06/2024

--- Parcial 3/spotify (1).sql
+++ Parcial 3/spotify (1).sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.8.5
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 30-05-2024 a las 04:40:33
--- Versión del servidor: 10.1.39-MariaDB
--- Versión de PHP: 7.3.5
+-- Tiempo de generación: 05-06-2024 a las 02:53:11
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -32,14 +31,23 @@ CREATE TABLE `artistas` (
   `id` int(11) NOT NULL,
   `nombre` varchar(100) NOT NULL,
   `biografia` varchar(300) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
 -- Volcado de datos para la tabla `artistas`
 --
 
 INSERT INTO `artistas` (`id`, `nombre`, `biografia`) VALUES
-(1, 'Cornelio Vega y su dinastia', 'El carisma y talento de la agrupacion les ha ganado millones de vistas en Youtube');
+(1, 'Cornelio Vega y su dinastia', 'El carisma y talento de la agrupacion les ha ganado millones de vistas en Youtube'),
+(2, 'Olivia Rodrigo', 'Es una actriz, cantante y compositora estadounidense. '),
+(3, 'EME15', 'Banda de pop latino mexicana compuesta por los seis actores principales de la serie de televisión de 2012, Miss XV de Nickelodeon Latinoamérica.'),
+(4, 'Chase Atlantic', 'Es una banda australiana de R&B alternativo y un trío de producción de Cairns, Australia, formado en 2014.'),
+(5, 'Sergio Vega', 'Conocido también como El Shaka, fue un cantante de música regional mexicana; especializado en los estilos de banda sinaloense y norteño.'),
+(6, 'Trueno', 'Mateo Palacios Corazzina (Buenos Aires, 25 de marzo de 2002), conocido por su nombre artístico Trueno, es un rapero y compositor argentino.'),
+(7, 'Natanael Cano', 'Cantante y compositor mexicano dedicado principalmente al género musical corridos tumbados, además de ser acreditado como precursor del mismo.'),
+(8, 'El Coyote', 'José Ángel Ledesma Quintero, (1 de noviembre de 1970, Mazatlán, Sinaloa) más conocido como \"El Coyote\", es un cantante mexicano de banda sinaloense.'),
+(9, 'The Neighbourhood\r\n', 'Es una banda de rock alternativo estadounidense creada en agosto de 2011.'),
+(10, 'Romeo Santos', 'Es un cantante, productor discográfico, compositor, actor y artista discográfico estadounidense. Es considerado en la mayor parte de América como el Rey de la Bachata, por ser el líder, vocalista y compositor principal de la agrupación Aventura.');
 
 -- --------------------------------------------------------
 
@@ -51,7 +59,14 @@ CREATE TABLE `artistas_canciones` (
   `id` int(11) NOT NULL,
   `artista_id` int(11) NOT NULL,
   `cancion_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `artistas_canciones`
+--
+
+INSERT INTO `artistas_canciones` (`id`, `artista_id`, `cancion_id`) VALUES
+(4, 1, 123);
 
 -- --------------------------------------------------------
 
@@ -67,7 +82,25 @@ CREATE TABLE `canciones` (
   `fecha` date NOT NULL,
   `activo` tinyint(1) NOT NULL,
   `foto` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `canciones`
+--
+
+INSERT INTO `canciones` (`id`, `nombre`, `genero_id`, `duracion`, `fecha`, `activo`, `foto`) VALUES
+(13, 'Sin ti', 1, '00:02:56', '2024-05-21', 1, ''),
+(15, 'No me hubiera enamorado', 1, '00:03:27', '2017-08-12', 1, ''),
+(16, 'Fue un error amarte', 1, '00:02:44', '2018-06-27', 1, ''),
+(17, 'El problema', 1, '00:03:04', '2017-11-10', 1, ''),
+(18, 'El primero', 1, '00:02:42', '2016-10-29', 1, ''),
+(19, 'Como le explico', 1, '00:02:55', '2023-11-03', 1, ''),
+(20, 'Yo soy', 1, '00:02:58', '2017-06-01', 1, ''),
+(21, 'Creí', 1, '00:02:37', '2024-05-04', 1, ''),
+(22, 'Haz lo que quieras', 1, '00:03:17', '2020-02-29', 1, ''),
+(23, 'Como este cabrón', 1, '00:03:08', '2024-05-01', 1, ''),
+(30, 'Reflections', 4, '00:04:04', '2018-04-09', 1, ''),
+(31, 'So american', 3, '00:02:49', '2023-09-08', 1, '');
 
 -- --------------------------------------------------------
 
@@ -79,7 +112,7 @@ CREATE TABLE `favoritas` (
   `id` int(11) NOT NULL,
   `usuario_id` int(11) NOT NULL,
   `cancion_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -90,7 +123,20 @@ CREATE TABLE `favoritas` (
 CREATE TABLE `generos` (
   `id` int(11) NOT NULL,
   `nombre` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `generos`
+--
+
+INSERT INTO `generos` (`id`, `nombre`) VALUES
+(1, 'Musica Norteña'),
+(2, 'Reggaeton'),
+(3, 'Pop'),
+(4, 'Rock'),
+(5, 'Rap'),
+(7, 'Bachata'),
+(8, 'Corridos Tumbados');
 
 -- --------------------------------------------------------
 
@@ -102,7 +148,16 @@ CREATE TABLE `membresias` (
   `id` int(11) NOT NULL,
   `descripcion` varchar(100) NOT NULL,
   `precio` double NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `membresias`
+--
+
+INSERT INTO `membresias` (`id`, `descripcion`, `precio`) VALUES
+(4, 'Individual', 129),
+(5, 'Dúo', 169),
+(6, 'Familiar', 199);
 
 -- --------------------------------------------------------
 
@@ -118,7 +173,7 @@ CREATE TABLE `playlist` (
   `total_canciones` int(11) NOT NULL,
   `descripcion` varchar(200) NOT NULL,
   `public` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -130,7 +185,7 @@ CREATE TABLE `playlist_canciones` (
   `id` int(11) NOT NULL,
   `playlist_id` int(11) NOT NULL,
   `cancion_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
 
@@ -143,7 +198,23 @@ CREATE TABLE `usuarios` (
   `nombre` varchar(100) NOT NULL,
   `correo` varchar(150) NOT NULL,
   `membresias_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `membresias_id`) VALUES
+(4, 'Camila García Osuna', 'osunac551@gmail.com', 4),
+(6, 'Claudia Osuna Ramírez', 'claudiaosuna@gmail.com', 6),
+(26, 'Ximena García Osuna', 'ximegarcia@gmail.com', 5),
+(27, 'Ivan Garcia Lizarraga', 'ivangarcializ@gmail.com', 6),
+(29, 'Andrea Salas Beltran', 'andreasalas@gmail.com', 4),
+(30, 'Ekaín Cervera Rodríguez', 'ekaincervera23@gmail.com', 5),
+(32, 'Marco Uriel Aguiluz Ibarra', 'marcoaguiluz12@gmail.com', 6),
+(33, 'Angel Santiago Pou Mapula', 'poumapula01@gmail.com', 4),
+(34, 'Diego Ángel Chávez Guerra', 'diegochavez.38@gmail.com', 5),
+(35, 'Aurelio Pérez Beltrán', 'aureliobel60@gmail.com', 4);
 
 --
 -- Índices para tablas volcadas
@@ -217,19 +288,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `artistas`
 --
 ALTER TABLE `artistas`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `artistas_canciones`
 --
 ALTER TABLE `artistas_canciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT de la tabla `canciones`
 --
 ALTER TABLE `canciones`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- AUTO_INCREMENT de la tabla `favoritas`
@@ -241,13 +312,13 @@ ALTER TABLE `favoritas`
 -- AUTO_INCREMENT de la tabla `generos`
 --
 ALTER TABLE `generos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `membresias`
 --
 ALTER TABLE `membresias`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT de la tabla `playlist`
@@ -265,7 +336,7 @@ ALTER TABLE `playlist_canciones`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
 
 --
 -- Restricciones para tablas volcadas
@@ -275,27 +346,19 @@ ALTER TABLE `usuarios`
 -- Filtros para la tabla `artistas_canciones`
 --
 ALTER TABLE `artistas_canciones`
-  ADD CONSTRAINT `artistas_canciones_ibfk_1` FOREIGN KEY (`artista_id`) REFERENCES `artistas` (`id`),
-  ADD CONSTRAINT `artistas_canciones_ibfk_2` FOREIGN KEY (`cancion_id`) REFERENCES `canciones` (`id`);
+  ADD CONSTRAINT `artistas_canciones_ibfk_1` FOREIGN KEY (`artista_id`) REFERENCES `artistas` (`id`);
 
 --
 -- Filtros para la tabla `canciones`
 --
 ALTER TABLE `canciones`
-  ADD CONSTRAINT `canciones_ibfk_1` FOREIGN KEY (`id`) REFERENCES `generos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `canciones_ibfk_2` FOREIGN KEY (`genero_id`) REFERENCES `artistas_canciones` (`id`);
+  ADD CONSTRAINT `canciones_ibfk_1` FOREIGN KEY (`genero_id`) REFERENCES `generos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `favoritas`
 --
 ALTER TABLE `favoritas`
   ADD CONSTRAINT `favoritas_ibfk_1` FOREIGN KEY (`cancion_id`) REFERENCES `canciones` (`id`);
-
---
--- Filtros para la tabla `membresias`
---
-ALTER TABLE `membresias`
-  ADD CONSTRAINT `membresias_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuarios` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 --
 -- Filtros para la tabla `playlist_canciones`
@@ -307,7 +370,7 @@ ALTER TABLE `playlist_canciones`
 -- Filtros para la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`membresias_id`) REFERENCES `playlist` (`id`);
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`membresias_id`) REFERENCES `membresias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
