402297912ba5aa89338315b9f7ccb368e2198c4f
CamiOsuna <159404147+CamiOsuna@users.noreply.github.com>
2024-09-20 22:07:51 -0600

    Práctica 4 mejorada García Osuna Camila 5AVP

--- BasedeDatosWeb/Parcial1/practica4.php
+++ BasedeDatosWeb/Parcial1/practica4.php
@@ -33,6 +33,72 @@
             $aciertos++;
         }
     }
+    //valida pregunta 4 4. ¿Cuál es el poder especial de Alice?
+    echo "<h3> ¿Cuál es el poder especial de Alice? </h3>";
+    echo "<h5>Repuesta seleccionada ".$pregunta4." ---- Correcta = Ver el futuro</h5>";
+    if($pregunta4 == "Torturar con la mirada"){
+        $aciertos++;
+        echo "<img src='correcta.jpg' width='50px'><hr>";
+    } else {
+        echo "<img src='incorrecta.png' width='50px'><hr>";
+    }
+
+    //valida pregunta 5 5. ¿Cuál es el poder especial de Edward?
+    echo "<h3> ¿Cuál es el poder especial de Edward? </h3>";
+    echo "<h5>Repuesta seleccionada ".$pregunta5." ---- Correcta = Leer la mente</h5>";
+    if($pregunta5 == "Torturar con la mirada"){
+        $aciertos++;
+        echo "<img src='correcta.jpg' width='50px'><hr>";
+    } else {
+        echo "<img src='incorrecta.png' width='50px'><hr>";
+    }
+
+    //Valido pregunta 6 s Tiene superfuerza
+    if(strtoupper($pregunta6) == "Emmett"){
+        $aciertos++;
+    }
+
+    //Valido pregunta 7 s Tiene 4 hermanos adoptivos
+    if(strtoupper($pregunta7) == "Edward"){
+        $aciertos++;
+    }
+
+    //Pregunta 8 - se deben validar 2 opciones Rosalie y Alice
+    $p8_respuestas_correctas = 0;
+
+    if(count($pregunta8) == 2){
+        for($i=0; $i<count($pregunta8); $i++){
+            if($pregunta8[$i] == "Rosalie" || $pregunta8[$i] == "Alice"){
+                $p8_respuestas_correctas ++;
+            }
+        }
+        if($p8_respuestas_correctas == 2){
+            $aciertos++;
+        }
+    }
+
+       //Pregunta 9 - se deben validar 2 opciones Emmett y Jasper
+       $p9_respuestas_correctas = 0;
+
+       if(count($pregunta9) == 2){
+           for($i=0; $i<count($pregunta9); $i++){
+               if($pregunta9[$i] == "Emmett" || $pregunta9[$i] == "Jasper"){
+                   $p9_respuestas_correctas ++;
+               }
+           }
+           if($p9_respuestas_correctas == 2){
+               $aciertos++;
+           }
+       }
+
+     //Valido pregunta 10 s Es la figura materna de la familia Cullen
+     if(strtoupper($pregunta10) == "Esme"){
+        $aciertos++;
+    }
+
+
+
+ 
     
     echo "CALIFICACIÓN FINAl = " . ($aciertos * 10) . "%";
 
