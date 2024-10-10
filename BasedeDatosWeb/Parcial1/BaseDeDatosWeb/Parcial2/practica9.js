27fb092845671be2c17e5b864889886c846698a2
CamiOsuna <159404147+CamiOsuna@users.noreply.github.com>
2024-10-09 18:18:25 -0700

    García Osuna Camila 5AVP

---
+++ BasedeDatosWeb/Parcial1/BaseDeDatosWeb/Parcial2/practica9.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    cartas = new Array(54).fill(false);
+    //Función click del boton dar carta
+    $("#dar_carta").click(function(){
+        var b = true;
+        var num = Math.floor(Math.random()*54) + 1;
+        console.log("Numero de carta ="+num);
+        while(b){
+            if(cartas[num-1] == true){
+            
+            }else{
+                $("#carta_activa").html("<img src='imagenes/"+num+".jpeg'>");
+                cartas[num-1] = true;
+                b=false;
+                cont ++;
+            }
+            if(cont == 54){
+                console.log("ya se dieron todas las cartas");
+                b=false;
+            }
+        }
+    });
+});
